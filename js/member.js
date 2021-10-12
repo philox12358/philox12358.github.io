24339e742f55bd361a0d93c6acb3f20f4896f01f
Jason Cheng <63398865+philox12358@users.noreply.github.com>
2021-10-12 13:35:24 +0800

    Add files via upload

---
+++ js/member.js
@@ -0,0 +1,46 @@
+var member = [
+  {
+    "phone": "No.2038",
+    "name": "星野冥一"
+  },
+  {
+    "phone": "No.0282",
+    "name": "Mr.Q"
+  },
+  {
+    "phone": "No.3392",
+    "name": "一般人类"
+  },
+  {
+    "phone": "No.1657",
+    "name": "湛蓝之炎"
+  },
+  {
+    "phone": "No.2802",
+    "name": "迷茫的虫"
+  },
+  {
+    "phone": "No.8920",
+    "name": "欺霜胜雪"
+  },
+  {
+    "phone": "No.1822",
+    "name": "北方丈助"
+  },
+  {
+    "phone": "No.9032",
+    "name": "「沙耶の唄」"
+  },
+  {
+    "phone": "No.5723",
+    "name": "adfs88"
+  },
+  {
+    "phone": "No.6327",
+    "name": "cyc123"
+  },
+  {
+    "phone": "No.5311",
+    "name": "サリアの歌"
+  }
+]
